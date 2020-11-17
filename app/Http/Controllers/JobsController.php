292b7d259e27cbec187b3d485737878632d5f815
Fer Rovito <fer.rovito@gmail.com>
2020-11-17 19:00:06 +0100

    Jobs controller and Model finished

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -3,11 +3,128 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\ModerationMail;
+use App\Job;
+
+use Redirect, Response;
 
 class JobsController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
-        return view('pages.jobs');
+        $jobs = Job::where('status','Published')->get();
+        return view('pages.jobs')->with("jobs",$jobs);
+    }
+    
+    public function moderate($id)
+    {
+        // Accept or decline proposal
+        
+        $job = Job::find($id);
+        $job->status = "Published";
+        $job->save();
+        return redirect('jobs');
+        //return view('pages.jobs')->with("jobs",$jobs);
+    }
+    
+    
+    public function spam($id)
+    {
+        // Accept or decline proposal
+        
+        $job = Job::find($id);
+        $job->status = "Spam";
+        $job->save();
+        return redirect('jobs');
+        //return view('pages.jobs')->with("jobs",$jobs);
+    }
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('pages.jobs_create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'email' => 'required|email'
+        ]);
+
+        $exists = Job::where('email',$data['email'])->get();
+        if($exists->count() == 0){
+            // Email
+            $data['status'] = "Moderate";
+        }
+
+        $success = Job::create($data);
+        
+        $data['id'] = $success->id;
+        Mail::to('fer.rovito@gmail.com')->send(new ModerationMail($data));
+
+        return redirect('jobs');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
     }
 }
--- resources/views/pages/jobs.blade.php
+++ resources/views/pages/jobs.blade.php
@@ -5,16 +5,30 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
+              <div class="card-body">
+                <h3 style="display:inline-block;">Available positions</h3>
+                <a href="/jobs/create">
+                    <button type="button" class="btn btn-primary btn-sm float-right">Publish Job</button>
+                </a>  
+              </div>
+            </div>
+            @foreach ($jobs as $job)
+            <div class="card">
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card text-center">
+                        <div class="card-header">
+                          {{ $job->title }}
                         </div>
-                    @endif
-
-                    Jobs
+                        <div class="card-body">
+                          <p class="card-text">{{ $job->description }}</p>
+                        </div>
+                        <div class="card-footer text-muted">
+                          Published on {{ date('F j, Y', strtotime($job->created_at)) }}
+                        </div>
+                    </div>
                 </div>
             </div>
+            @endforeach
         </div>
     </div>
 </div>
